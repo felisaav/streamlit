267cdcd7443833226ed4d5f128aafeac38ae7fa5
Felipe Saavedra <felipe.saavedra@student.ie.edu>
2023-07-20 04:19:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,11 +33,13 @@ To *italicize text*, add one asterisk or underscore before and after a word or p
 Add three asterisks or underscores before and after a word or ***phrase for bold italic***
 ---
 #### Blockquotes
+
 To create a blockquote, add a > in front of a paragraph.
 >Dorothy followed her through many of the beautiful rooms in her castle.
 >>The Witch bade her clean the pots and kettles and sweep the floor and keep the fire fed with wood.
 ---
 #### List
+
 Order List (items with number)
 1.First item
 2.Second item
@@ -55,14 +57,17 @@ add dashes (-), asterisks (*), or plus signs (+) in front of line items.
 - Fourth item
 ---
 #### Links
+
 To create a link, enclose the link text in brackets and then follow it immediately with the URL in parentheses
 
 My favorite search engine is [Google](https://google.com).
 ---
 #### URLs and Email Address
+
 To quickly turn a URL or email address into a link, enclose it in angle brackets.
 <https://www.ie.edu/school-science-technology/programs/master-business-analytics-big-data/>
 <concepciond@faculty.ie.edu>
 ---
 #### Images
+
 To add an image, add an exclamation mark (!), followed by alt text in brackets, and the path or URL to the image asset in parentheses. You can optionally add a title in quotation marks after the path or URL. ![Mountains are beautiful](https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80)''')
