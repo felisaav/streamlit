98e280711a2d9630f8e7b88fe55f57d7ecf674f6
Felipe Saavedra <felipe.saavedra@student.ie.edu>
2023-07-20 03:39:34 -0400

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.write('I hate windows')
+st.write(pd.DataFrame({
+  'first_column':[1,2,3,4],
+  'second_column':[10,20,30,40]
+}]
