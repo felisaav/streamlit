22fdbff161fa3c90db1797c8a7c8d5a234807ee8
Felipe Saavedra <felipe.saavedra@student.ie.edu>
2023-07-20 03:56:53 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,12 @@ st.write(pd.DataFrame({
   'first_column':[1,2,3,4],
   'second_column':[10,20,30,40]
 }))
+
+data=pd.DataFrame({
+  'first_column':[5,6,7,8],
+  'second_column':[15,25,35,45]
+})
+
+st.write(data)
+
+
