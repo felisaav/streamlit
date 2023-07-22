6659e0c62abebf0ec61cd3b3255ce3e1fc7b5d60
Felipe Saavedra <felipe.saavedra@student.ie.edu>
2023-07-22 05:09:56 -0400

    Update streamlit_app3.py

--- streamlit_app3.py
+++ streamlit_app3.py
@@ -1,8 +1,12 @@
+$ pipreqs /home/project/location
+Successfully saved requirements file in /home/felisaav/streamlit/requirements.txt
+
 import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+
 
 df=pd.read_csv('tips.csv')
 st.dataframe(df.head(6))
@@ -19,6 +23,24 @@ with st.container():
   ax.pie(sex_dist, autopct='%0.2f%%',labels=['Male','Female'])
   st.pyplot(fig)
 
-fig,ax=plt.subplots()
-ax.bar(sex_dist.index,sex_dist)
-st.pyplot(fig)
+  fig,ax=plt.subplots()
+  ax.bar(sex_dist.index,sex_dist)
+  st.pyplot(fig)
+
+st.markdown('---')
+
+with st.container():
+  c1,c2=st.columns(2)
+
+  with c1:
+    fig,ax=plt.subplots()
+    ax.pie(sex_dist,autopct='%0.2f%%',labels=['Male','Female'])
+    st.pyplot(fig)
+
+  with c2:
+    fig,ax=plt.subplots()
+    ax.bar(sex_dist.index,sex_dist)
+    st.pyplot(fig)
+
+  with st.expander('Click to see the data'):
+    st.dataframe(sex_dist)
