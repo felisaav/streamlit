b84e402886064658c859e8c0cebfd9aec82371fa
Felipe Saavedra <felipe.saavedra@student.ie.edu>
2023-07-22 04:34:12 -0400

    Update streamlit_app2.py

--- streamlit_app2.py
+++ streamlit_app2.py
@@ -2,6 +2,10 @@ import streamlit st
 import pandas as pd
 import numpy as np
 
+import .matplotlib.pyplot as plt
+import seaborn as sns
+
+df=pd.read_csv('tips.csv')
+st.DataFrame(df.head(5))
+
 st.markdown('---')
-st.subheader('we will do a checkbox')
-st.checkbox('do you agreed with the terms and conditions')
