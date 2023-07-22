1371e3167246f5710cca2c65e553b2c8c83fbb74
Felipe Saavedra <felipe.saavedra@student.ie.edu>
2023-07-22 04:35:56 -0400

    Update streamlit_app2.py

--- streamlit_app2.py
+++ streamlit_app2.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-import .matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import seaborn as sns
 
 df=pd.read_csv('tips.csv')
