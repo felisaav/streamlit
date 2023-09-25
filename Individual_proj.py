0b4bf32a2b62b160d6d8639219769f619f9e60df
Felipe Saavedra <felipe.saavedra@student.ie.edu>
2023-09-24 21:01:27 -0300

    Update Individual_proj.py

--- Individual_proj.py
+++ Individual_proj.py
@@ -5,6 +5,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
+import plotly.graph_objects as go
 import seaborn as sns
 import matplotlib.pyplot as plt
 import nltk
