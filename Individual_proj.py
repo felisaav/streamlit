6f7c645fd8198af7ceaff388a5ed0a3a72e5a897
Felipe Saavedra <felipe.saavedra@student.ie.edu>
2023-09-27 16:14:02 -0300

    Update Individual_proj.py

--- Individual_proj.py
+++ Individual_proj.py
@@ -78,8 +78,8 @@ predictions = model.predict(vectorizer.transform(X_test))
 #calculate accuracy of the model
 accuracy=100 * sum(predictions == Y_test) / len(predictions)
 precision = 100*sum(cm.loc[1,'1']/(cm.loc[0,'1']+cm.loc[1,'1']) #TP/Predicted positives
-specificity = 0 #TN/negatives(TN+FP)
-Recall= 0 #TP/positives(TP+FN)
+#specificity = 0 #TN/negatives(TN+FP)
+#Recall= 0 #TP/positives(TP+FN)
 
 cm = pd.DataFrame(confusion_matrix(Y_test, predictions))
 #class_report=pd.DataFrame(classification_report(Y_test, predictions))
@@ -206,8 +206,8 @@ def main():
 			st.dataframe(cm)
 			st.write(f"Accuracy: {accuracy:.2f}%")
 			st.write(f"Precision: {precision:.2f}%")
-			st.write(f"Specificity: {specificity:.2f}%")
-			st.write(f"Recall: {recall:.2f}%")
+			#st.write(f"Specificity: {specificity:.2f}%")
+			#st.write(f"Recall: {recall:.2f}%")
 		st.markdown("""---""")
 		st.subheader("Test a new email and see if it is a spam or not")
 		with st.form(key='test_email'):
